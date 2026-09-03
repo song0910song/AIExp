ccb45fe3e718556478a3093b13a76feb8c615347
songht0910 <1409024971@qq.com>
2026-09-03 22:09:41 +0800

    Revert "feat: implement luminaire layout analysis and report parsing"
    This reverts commit deb04d8c5bdcfc0645c928b183c50915a4e2eef2.

--- src/lighting_agent/calculations/__init__.py
+++ src/lighting_agent/calculations/__init__.py
@@ -7,7 +7,6 @@ from .preview import (
     PreviewGeometryError,
     compute_illuminance_preview,
 )
-from .layout import DEFAULT_COORDINATE_TOLERANCE_M, analyze_luminaire_layout
 
 __all__ = [
     "SOLVER_VERSION",
@@ -20,6 +19,4 @@ __all__ = [
     "check_design_rules",
     "compute_illuminance_preview",
     "parse_photometry",
-    "DEFAULT_COORDINATE_TOLERANCE_M",
-    "analyze_luminaire_layout",
 ]
